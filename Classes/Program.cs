2acc54d0caa6d555e07f7ae9e49017a6b14b56a7
kormonditamas2222 <t.kormond2006@gmail.com>
2025-02-07 10:49:04 +0100

    Könyv osztály példányosítva, metódusok használva

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,9 +2,24 @@
 {
 	internal class Program
 	{
+		static void Konyvek()
+		{
+			Konyv k1 = new Konyv("Egri csillagok", "Gárdonyi Géza", 1901, 555, 4500);
+			Konyv k2 = new Konyv("Életem", "Én");
+            Console.WriteLine(k1);
+			k1.Arnoveles(500);
+            Console.WriteLine(k1.Ar);
+			k1.Kedvezmeny(0.2);
+            Console.WriteLine(k1.Ar);
+            Console.WriteLine(k1.HanyOldalMaradt(241));
+            Console.WriteLine(k2);
+			k2.Arnoveles(1000);
+			k2.Kedvezmeny(0.01);
+            Console.WriteLine(k2);
+        }
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Konyvek();
 		}
 	}
 }
