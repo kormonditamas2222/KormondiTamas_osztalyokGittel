6e03dffe613a348614de774ebcb1dca11bf83d1b
kormonditamas2222 <t.kormond2006@gmail.com>
2025-02-07 11:08:35 +0100

    Film osztály példányosítva

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Classes
+﻿using System.Runtime.CompilerServices;
+
+namespace Classes
 {
 	internal class Program
 	{
@@ -17,9 +19,22 @@
 			k2.Kedvezmeny(0.01);
             Console.WriteLine(k2);
         }
+		static void Filmek()
+		{
+			Film f1 = new Film("Shrek", "Andrew Adamson, Vicky Jenson", 90, "Animációs film", true);
+			Film f2 = new Film("Tutel", "Petrik Lajos");
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+			f2.MufajModositas("Dokumentumfilm");
+			f2.HosszNovelese(20);
+			f2.Cim = "Iskola Budapesten";
+            Console.WriteLine(f2);
+			f1.Jatszas();
+        }
 		static void Main(string[] args)
 		{
 			Konyvek();
+			Filmek();
 		}
 	}
 }
